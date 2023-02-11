0e04e4a637b82dfe7f778693e4fdca6196f71e6d
sStarkmaXx <60282152+sStarkmaXx@users.noreply.github.com>
2023-02-11 14:09:56 +0700

    add pagination

--- src/pages/CardsView.tsx
+++ src/pages/CardsView.tsx
@@ -1,16 +1,39 @@
 import { v1 } from 'uuid';
 import ImageCard from '../components/ImageCard';
-import { Box } from '@mui/material';
+import { Box, Pagination } from '@mui/material';
 import { CardType } from '../store/slices/cards.slice';
+import { useRef, useState } from 'react';
 
 type CardsViewPropsType = {
   cards: CardType[];
 };
 
 const CardsView: React.FC<CardsViewPropsType> = ({ cards = [] }) => {
-  const cardsRender = cards.map((card) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const pageSize = 30;
+  const pagesCount = useRef(Math.ceil(cards.length / pageSize));
+
+  const cardsForRender = () => {
+    const lastIndex = pageSize * currentPage - 1;
+    let firstIndex = lastIndex - pageSize + 1;
+    const cardsForRender = [];
+    for (let i = firstIndex; i <= lastIndex; i++) {
+      if (cards[i]) {
+        cardsForRender.push(cards[i]);
+      } else {
+        return;
+      }
+    }
+    return cardsForRender;
+  };
+
+  const cardsRender = cardsForRender()?.map((card) => {
     return <ImageCard key={v1()} card={card} />;
   });
+
+  const onChangeHandler = (e: React.ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+  };
 
   return (
     <Box
@@ -22,6 +45,7 @@ const CardsView: React.FC<CardsViewPropsType> = ({ cards = [] }) => {
       }}
     >
       {cardsRender}
+      <Pagination count={pagesCount.current} onChange={onChangeHandler} />
     </Box>
   );
 };
